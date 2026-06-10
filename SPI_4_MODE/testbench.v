a471a889fa8e59657c623e3bba9b212c4d5a975f
SkinnyFarm <phamtrieukhangduy0337813356@gmail.com>
2026-06-10 14:05:51 +0700

    add

--- SPI_4_MODE/testbench.v
+++ SPI_4_MODE/testbench.v
@@ -2,34 +2,19 @@
 `include "design.v"
 
 module spi_master_tb;
-
-    // Inputs
     reg        clk;
     reg        rst;
     reg        start;
     reg  [1:0] mode;
     reg  [7:0] data_in;
 
-    // Outputs
     wire       mosi;
     wire       sclk;
     wire       cs;
     wire       done;
 
-    // Instantiate UUT
-    spi_master uut (
-        .clk     (clk),
-        .rst     (rst),
-        .start   (start),
-        .mode    (mode),
-        .data_in (data_in),
-        .mosi    (mosi),
-        .sclk    (sclk),
-        .cs      (cs),
-        .done    (done),
-    );
+    spi_master uut ( .clk(clk), .rst(rst), .start(start), .mode(mode), .data_in(data_in), .mosi(mosi), .sclk(sclk), .cs(cs), .done(done),);
 
-    // Clock 100 MHz
     initial clk = 0;
     always #5 clk = ~clk;
 
@@ -37,37 +22,36 @@ module spi_master_tb;
         $dumpfile("design.vcd");
         $dumpvars(0, spi_master_tb);
 
-        // Khoi tao
-        rst     = 1;    // Bat reset
+        rst     = 1; // Reset
         start   = 0;
         mode    = 2'b00;
         data_in = 8'h00;
         #40;
-        rst = 0;        // Tha reset -> DUT bat dau hoat dong
+        rst = 0;      
         #40;
 
-        // ---- TC1: Mode 0 (CPOL=0, CPHA=0) | data = 0xA5 ----
+        //Mode 0 (CPOL=0, CPHA=0)
         mode    = 2'b00;
         data_in = 8'hA5;
         #40;
         start = 1; #10; start = 0;
         #1200;
 
-        // ---- TC2: Mode 1 (CPOL=0, CPHA=1) | data = 0x3C ----
+        //Mode 1 (CPOL=0, CPHA=1)
         mode    = 2'b01;
         data_in = 8'h3C;
         #40;
         start = 1; #10; start = 0;
         #1200;
 
-        // ---- TC3: Mode 2 (CPOL=1, CPHA=0) | data = 0x69 ----
+        //Mode 2 (CPOL=1, CPHA=0)
         mode    = 2'b10;
         data_in = 8'h69;
         #40;
         start = 1; #10; start = 0;
         #1200;
 
-        // ---- TC4: Mode 3 (CPOL=1, CPHA=1) | data = 0xF0 ----
+        //Mode 3 (CPOL=1, CPHA=1)
         mode    = 2'b11;
         data_in = 8'hF0;
         #40;
