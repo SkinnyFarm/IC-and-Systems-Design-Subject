1e515ae540a0a8d5654ae151fd73685315beccd0
SkinnyFarm <phamtrieukhangduy0337813356@gmail.com>
2026-06-10 16:37:24 +0700

    modify

--- SPI_4_MODE/testbench.v
+++ SPI_4_MODE/testbench.v
@@ -13,7 +13,7 @@ module spi_master_tb;
     wire       cs;
     wire       done;
 
-    spi_master uut ( .clk(clk), .rst(rst), .start(start), .mode(mode), .data_in(data_in), .mosi(mosi), .sclk(sclk), .cs(cs), .done(done),);
+    spi_master uut ( .clk(clk), .rst(rst), .start(start), .mode(mode), .data_in(data_in), .mosi(mosi), .sclk(sclk), .cs(cs), .done(done));
 
     initial clk = 0;
     always #5 clk = ~clk;
@@ -28,35 +28,35 @@ module spi_master_tb;
         data_in = 8'h00;
         #40;
         rst = 0;      
-        #40;
+        #1000;
 
         //Mode 0 (CPOL=0, CPHA=0)
         mode    = 2'b00;
         data_in = 8'hA5;
         #40;
         start = 1; #10; start = 0;
-        #1200;
+        #00;
 
         //Mode 1 (CPOL=0, CPHA=1)
         mode    = 2'b01;
         data_in = 8'h3C;
         #40;
         start = 1; #10; start = 0;
-        #1200;
+        #1000;
 
         //Mode 2 (CPOL=1, CPHA=0)
         mode    = 2'b10;
         data_in = 8'h69;
         #40;
         start = 1; #10; start = 0;
-        #1200;
+        #1000;
 
         //Mode 3 (CPOL=1, CPHA=1)
         mode    = 2'b11;
         data_in = 8'hF0;
         #40;
         start = 1; #10; start = 0;
-        #1200;
+        #1000;
 
         $finish;
     end
